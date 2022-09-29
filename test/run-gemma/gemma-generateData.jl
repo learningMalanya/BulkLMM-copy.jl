468364df334dae25ce190484c83b89abacd1a47f
Zifan Yu <zfredyu@uw.edu>
2022-09-29 14:06:26 -0500

    Prepare for GEMMA test.

---
+++ test/run-gemma/gemma-generateData.jl
@@ -0,0 +1,14 @@
+# Will be using BXD data
+## Include the script to generate data
+include("../../src/readData.jl");
+
+## Read in BXD data:
+pheno_file = "../../data/bxdData/BXDtraits.csv"
+pheno = readBXDpheno(pheno_file);
+geno_file = "../../data/bxdData/BXDgeno_prob.csv"
+geno = readGenoProb_ExcludeComplements(geno_file);
+
+
+## Read in BXD data to the BIMBAM format:
+transform_bxd_geno_to_gemma(geno_file, "BXDgeno_prob_bimbam.txt");
+transform_bxd_pheno_to_gemma(pheno_file, "BXDpheno_bimbam.txt", 7919);
