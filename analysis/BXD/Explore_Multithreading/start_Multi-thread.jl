9405a5e38a9c6952f542f878bbfdeb049c9b53b7
Zifan Yu <zfredyu@uw.edu>
2022-10-20 15:06:51 -0500

    Saved work on bulkscan.

--- analysis/BXD/Explore_Multithreading/start_Multi-thread.jl
+++ analysis/BXD/Explore_Multithreading/start_Multi-thread.jl
@@ -1,4 +1,4 @@
 using IJulia
-IJulia.installkernel("Julia 24 Threads", env=Dict(
-    "JULIA_NUM_THREADS" => "24",
+IJulia.installkernel("Julia 4 Threads", env=Dict(
+    "JULIA_NUM_THREADS" => "4",
 ))
